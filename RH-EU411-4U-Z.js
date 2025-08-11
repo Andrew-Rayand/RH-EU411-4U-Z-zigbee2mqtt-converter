fc1f20f8d0e7c0e7e5119f49d0ceec57dd97e6aa
Andrew Rayand <116679297+Andrew-Rayand@users.noreply.github.com>
2025-08-12 00:26:42 +0300

    Update RH-EU411-4U-Z.js
    Исправления для раздельного управления

--- RH-EU411-4U-Z.js
+++ RH-EU411-4U-Z.js
@@ -1,4 +1,4 @@
-// Данный конвертер предназначен для блока розеток Tuya RH-EU411-4U-Z нужно ввести только IEEE-адрес
+// Данный конвертер предназначен для блока розеток Tuya RH-EU411-4U-Z - нужно ввести только IEEE-адрес
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
@@ -6,43 +6,63 @@ const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 
 const e = exposes.presets;
 
-module.exports = {
+const definition = {
     fingerprint: [
         {
-            ieeeAddr: '0xa0c00000d0e0c000',  // конкретный IEEE-адрес устройства
+            ieeeAddr: '0xa0c00000d0e0c000',   // уникальный адрес твоего устройства
             modelID: 'TS011F',
-            manufacturerName: '_TZ3000_pl5v1yyy',
+            manufacturerName: '_TZ3000_pl5v1yyy'
         }
     ],
     model: 'E220-KR4N0Z0-HA',
-    vendor: 'TuYa',
-    description: '4 AC outlets + 4 USB ports',
-    fromZigbee: [fz.on_off],
+    vendor: 'Tuya',
+    description: '4 AC outlets + 4 USB ports with power monitoring (separate control fix)',
+
+    fromZigbee: [fz.on_off, fz.electrical_measurement],
     toZigbee: [tz.on_off],
+
     exposes: [
         e.switch().withEndpoint('l1'),
         e.switch().withEndpoint('l2'),
         e.switch().withEndpoint('l3'),
         e.switch().withEndpoint('l4'),
-        e.switch().withEndpoint('l5'),
+        e.switch().withEndpoint('l5').withDescription('USB ports'),
+        e.power(),
+        e.current(),
+        e.voltage(),
     ],
+
     meta: {
         multiEndpoint: true,
+        tuya_do_not_publish_state: true, // чтобы команды не перезаписывались
     },
-    endpoint: (device) => {
-        return {
-            'l1': 1,
-            'l2': 2,
-            'l3': 3,
-            'l4': 4,
-            'l5': 5
-        };
-    },
+
+    endpoint: (device) => ({
+        'l1': 1,
+        'l2': 2,
+        'l3': 3,
+        'l4': 4,
+        'l5': 5,
+    }),
+
     configure: async (device, coordinatorEndpoint, logger) => {
-        for (let i = 1; i <= 5; i++) {
+        // Включаем энергомониторинг и управление для первой розетки
+        const endpoint1 = device.getEndpoint(1);
+        await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement']);
+        await reporting.onOff(endpoint1);
+        await reporting.rmsVoltage(endpoint1, {min: 10, max: 3600, change: 5});
+        await reporting.rmsCurrent(endpoint1, {min: 10, max: 3600, change: 10});
+        await reporting.activePower(endpoint1, {min: 10, max: 3600, change: 2});
+
+        // Настройка управления для остальных розеток
+        for (let i = 2; i <= 5; i++) {
             const endpoint = device.getEndpoint(i);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
-            await reporting.onOff(endpoint);
+            if (endpoint) {
+                await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+                await reporting.onOff(endpoint);
+            }
         }
     },
 };
+
+module.exports = definition;
