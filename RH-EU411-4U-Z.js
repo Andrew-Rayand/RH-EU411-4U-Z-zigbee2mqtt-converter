dfcdb6a594647948cdebae74536e206db4014022
Andrew Rayand <116679297+Andrew-Rayand@users.noreply.github.com>
2025-08-12 20:31:05 +0300

    Update RH-EU411-4U-Z.js
    Исправлена логика работы.
    Розетки теперь корректно управляется по отдельности.

--- RH-EU411-4U-Z.js
+++ RH-EU411-4U-Z.js
@@ -1,4 +1,4 @@
-// Данный конвертер предназначен для блока розеток Tuya RH-EU411-4U-Z - нужно ввести только IEEE-адрес
+// Данный конвертер предназначен для блока розеток Tuya RH-EU411-4U-Z
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
@@ -9,14 +9,13 @@ const e = exposes.presets;
 const definition = {
     fingerprint: [
         {
-            ieeeAddr: '0xa0c00000d0e0c000',   // уникальный адрес твоего устройства
             modelID: 'TS011F',
-            manufacturerName: '_TZ3000_pl5v1yyy'
+            manufacturerName: '_TZ3000_pl5v1yyy',
         }
     ],
     model: 'E220-KR4N0Z0-HA',
     vendor: 'Tuya',
-    description: '4 AC outlets + 4 USB ports with power monitoring (separate control fix)',
+    description: '4 AC outlets + 4 USB ports with separate control fix',
 
     fromZigbee: [fz.on_off, fz.electrical_measurement],
     toZigbee: [tz.on_off],
@@ -34,7 +33,6 @@ const definition = {
 
     meta: {
         multiEndpoint: true,
-        tuya_do_not_publish_state: true, // чтобы команды не перезаписывались
     },
 
     endpoint: (device) => ({
@@ -46,22 +44,11 @@ const definition = {
     }),
 
     configure: async (device, coordinatorEndpoint, logger) => {
-        // Включаем энергомониторинг и управление для первой розетки
         const endpoint1 = device.getEndpoint(1);
-        await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement']);
-        await reporting.onOff(endpoint1);
+        await reporting.bind(endpoint1, coordinatorEndpoint, ['haElectricalMeasurement']);
         await reporting.rmsVoltage(endpoint1, {min: 10, max: 3600, change: 5});
         await reporting.rmsCurrent(endpoint1, {min: 10, max: 3600, change: 10});
         await reporting.activePower(endpoint1, {min: 10, max: 3600, change: 2});
-
-        // Настройка управления для остальных розеток
-        for (let i = 2; i <= 5; i++) {
-            const endpoint = device.getEndpoint(i);
-            if (endpoint) {
-                await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
-                await reporting.onOff(endpoint);
-            }
-        }
     },
 };
 
